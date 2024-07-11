061fa1c1f28c58063f5f22f58ec7459740f801cc
ChaitraSuvarnaa <chaitrasuvarna4065@gmail.com>
2024-07-11 10:48:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('.topnav a');
+
+    for (const link of links) {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
